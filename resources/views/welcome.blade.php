bc488f404e3e8e53b2fd32ebc0934cc0c743b60e
PSNapier <46052087+PSNapier@users.noreply.github.com>
2025-04-29 16:06:37 -0500

    feat: added cta button, tweaked visuals

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,31 +13,37 @@
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+	  @php
+	  	$mailtoTemplate = 'mailto:abaturestudio@gmail.com?subject=New Client Enquiry';
+	  @endphp
     </head>
 	<body class="text-center max-w-[1080px] lg:text-left lg:m-auto lg:p-8 text-2xl flex flex-col [&>div]:mb-10 items-center">
-		<div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-4">
+		<div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-4 items-center">
 			<div class="max-h-[250px] mb-4 lg:mb-0 lg:max-h-[425px] lg:rounded-md overflow-hidden flex justify-center items-center">
 				<img src="{{ asset('images/basset-hound.webp') }}" alt="Basset Hound 🐾">
 			</div>
-			<div class="m-2 col-span-2 [&>*]:mb-2">
+
+			<div class="m-2 col-span-2 [&>*]:mb-4">
 				<div class="font-semibold text-3xl">Dog Walking & Pet Care</div>
-				<p>Rancho Sienna neighbor offering dog walking and personalized pet care!</p>
-				<p>I bring 20+ years of experience with dogs, including positive reinforcement and pressure-release training methods. I also have over 2 years professional experience as a supervisor at a luxury kennel caring for up to 200 dogs a day, managing feeding, medications, potty breaks, and playtime routines. I am experienced with canines, equines, small animals, and reptiles.</p>
-				<p>Solo walks ensure your pet’s safety, comfort, and my undivided attention. ✨</p>
+				<p>Rancho Sienna neighbor personalized pet care!</p>
+				<p>I bring over 20 years of experience with dogs, including 2 years professional experience as a supervisor at a luxury kennel caring for up to 200 dogs a day; managing feeding, medications, potty breaks, and playtime routines.</p>
+				<p>I am also experienced with equines, small animals, and reptiles.</p>
+				<a href="{{ $mailtoTemplate }}"><button class="ml-auto font-semibold bg-green-600 text-zinc-50 rounded-lg py-2 px-4 hover:scale-[105%] cursor-pointer hover:transition transition">Book your first walk for free!</button></a>
 			</div>
 		</div>
 
-		<div class="mx-2 grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-4 [&>div]:text-center">
+		<div class="mx-2 grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-4 [&>div]:text-center [&>div]:border-2 [&>div]:border-green-600 [&>div]:rounded-lg [&>div]:p-4">
 			<div>
 				<div class="font-semibold text-3xl">Walks</div>
-				<p>Private leashed walks around your neighborhood for exercise and enrichment.</p>
+				<p>Solo walks ensure your pet’s safety, comfort, and my undivided attention.</p>
 				<p class="font-semibold mt-4">🐾 $13 per 20 minutes</p>
 				<p class="italic">+$5 per additional pup</p>
 			</div>
 
 			<div>
 				<div class="font-semibold text-3xl">Check-Ins</div>
-				<p>Short visits for potty breaks, meals, or medication while you're away.</p>
+				<p>Short visits for potty breaks, meals and medication.</p>
 				<p class="font-semibold mt-4">🐶 $10 per visit</p>
 			</div>
 
@@ -50,7 +56,7 @@
 
 		<div class="mx-2 text-center">
 			<p class="font-semibold text-3xl">Contact Me</p>
-			<p><span class="font-semibold">Email:</span> <a class="underline hover:text-green-600 hover:transition transition" href="mailto:abaturestudio@gmail.com">AbatureStudio@gmail.com</a></p>
+			<p><span class="font-semibold">Email:</span> <a class="underline hover:text-green-600 hover:transition transition" href="{{ $mailtoTemplate }}">AbatureStudio@gmail.com</a></p>
 			<p><span class="font-semibold">Text:</span> 512-228-7795</p>
 		</div>
 
